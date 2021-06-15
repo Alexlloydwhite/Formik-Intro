4f7ebc70a3938faac0b8c5a1e1763d3b09ce0fd6
Alex White <alexlloydwhite@gmail.com>
2021-06-15 17:20:53 -0500

    adding form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { Button, TextField } from '@material-ui/core';
+import { CheckBox } from '@material-ui/icons';
+import { Field, Form, Formik } from "formik";
 import React from 'react';
-import { Formik, Field, Form } from "formik";
-import { TextField } from '@material-ui/core';
-import { Button } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
     <div>
       <Formik
-        initialValues={{ firstName: "", lastName: "" }}
+        initialValues={
+          {
+            firstName: "",
+            lastName: "",
+            isTall: false,
+            cookies: []
+          }
+        }
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
           // Make async call
@@ -15,14 +22,56 @@ const App: React.FC = () => {
           setSubmitting(false);
         }}
       >
-        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+        {(
+          {
+            values,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit
+          }
+        ) => (
           <Form>
             <div>
-              <Field placeholder="first name" name="firstName" type="input" as={TextField} />
+              <Field
+                placeholder="first name"
+                name="firstName"
+                type="input"
+                as={TextField}
+              />
             </div>
             <div>
-              <Field placeholder="last name" name="lastName" type="input" as={TextField} />
+              <Field
+                placeholder="last name"
+                name="lastName"
+                type="input"
+                as={TextField}
+              />
             </div>
+            <Field
+              name="isTall"
+              type="checkbox"
+              as={CheckBox}
+            />
+            <div>Cookies:</div>
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="chocolate chip"
+              as={CheckBox}
+            />
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="snickerdoodle"
+              as={CheckBox}
+            />
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="sugar"
+              as={CheckBox}
+            />
             <div>
               <Button
                 disabled={isSubmitting}
